470fc6b3d594568b74db14d731edee48a7fe67c8
jess <46928740+jessfin@users.noreply.github.com>
2024-04-16 10:07:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -155,4 +155,4 @@ app = web.Application()
 app.router.add_route("*", "/v1/chat/completions", onRequest)
 
 if __name__ == '__main__':
-    web.run_app(app, host='127.0.0.1', port=3030)
+    web.run_app(app, host='0.0.0.0', port=3030)
